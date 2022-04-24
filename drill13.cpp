b347e8a77b330425190c9985dce6dc358607a679
Kovács Dávid <d.david010313@gmail.com>
2022-04-24 20:00:03 +0200

    Update drill13.cpp

--- drill13.cpp
+++ drill13.cpp
@@ -7,14 +7,14 @@ using namespace Graph_lib;
 
 int main(){
 try {
-		Simple_window win{Point{300, 50}, 1000, 800, "My window"};
+		Simple_window win{Point{300, 50}, 1000, 800, "My window"}; //Ablak jön létre, 300-50 helyen, 1000*800-as
 		win.wait_for_button();
 
-		int x_size=800;
-		int y_size=800;
+		int x_size = 800;
+		int y_size = 800;
 
 		Lines grid;
-		for (int i = 100; i < x_size+1; i+=100)
+		for (int i = 100; i < x_size+1; i+=100) 
 		{
 			grid.add(Point{i, 0}, Point{i, y_size}); //vertical
 			grid.add(Point{0, i}, Point{x_size, i}); //horizontal
@@ -25,18 +25,18 @@ try {
 		win.wait_for_button();
 
 		Vector_ref<Rectangle> vr;
-    	for (int i = 0; i < 8; i++)
+    	for (int i = 0; i < 8; i++) //átlót rajzolja
     	{
 	        vr.push_back(new Rectangle{Point{i*100,i*100},100,100});
-	        vr[vr.size()-1].set_fill_color(Color::red);
-	        win.attach(vr[vr.size()-1]);
+	        vr[i].set_fill_color(Color::red);
+	        win.attach(vr[i]);
     	}
     	win.label("My Window(color)");
         win.wait_for_button();
 
 
 		Image img1 {Point{0,200},"badge.jpg"};
-		img1.set_mask(Point{0,200}, 200, 200);
+		img1.set_mask(Point{0,200}, 200, 200); //eredeti képhez nézve: 0-200 helyre teszi, levágja 200*200-as ra
 		win.attach(img1);
 		Image img2 {Point{200,400},"badge.jpg"};
 		img2.set_mask(Point{0,200}, 200, 200);
@@ -47,16 +47,16 @@ try {
 		win.label("My Window(3 img)");
 		win.wait_for_button();
 
-		Image img_move {Point{0, 0},"badge.jpg"};
+		Image img_move {Point{0, 0},"badge.jpg"}; //az eredeti helyről ennyivel tolja arrébb
 		img_move.set_mask(Point{0,200}, 100, 100);
 		win.attach(img_move);
 		win.label("My Window(img moving)");
 		win.wait_for_button();
 		while(true)
 		{
-        	for (int i = 0; i < 8; ++i)
+        	for (int i = 0; i < 8; ++i) // y-tengelyen nézve
         	{
-        		for (int j = 0; j < 7; ++j)
+        		for (int j = 0; j < 7; ++j) // x-tengelyen nézve
         		{
         			img_move.move(100,0);
         			win.label("My Window(x+100, y+0)");
